0f18b8dce3137ef6b49810fbaa688724d4da2b97
Natalia Ginalska <natalia.ginalska@gmail.com>
2019-02-01 13:08:29 +0100

    Implement of RWD on Home (ProductList)

--- src/modules/Product/ProductList.js
+++ src/modules/Product/ProductList.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
 import { connect } from 'react-redux';
 import ProductListItem from './ProductListItem';
 import Pagination from '../Pagination/Pagination';
@@ -6,22 +7,43 @@ import compareObjects from '../../util/compareObjects';
 
 import './ProductList.scss';
 
-const ProductList = ({ products, page }) => {
-  const itemsToPage = 6;
-  const rangeFrom = (page - 1) * itemsToPage;
-  const rangeTo = (page * itemsToPage);
-  const productsTab = products;
-  const subProductsTab = productsTab.slice(rangeFrom, rangeTo);
-  const pageCount = Math.ceil(productsTab.length/itemsToPage);
+class ProductList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemsToPage: 6,
+    };
+  }
 
-  return (
-    <div className="container">
-      <div className="ProductList">
-        {subProductsTab.map(product => <ProductListItem key={product.id} product={product} />)}
+  static getDerivedStateFromProps(nextProps, nextState) {
+    const rangeFrom = (nextProps.page - 1) * nextState.itemsToPage;
+    const rangeTo = (nextProps.page * nextState.itemsToPage);
+    const productsTab = nextProps.products;
+    const subProductsTab = productsTab.slice(rangeFrom, rangeTo);
+    const pageCount = Math.ceil(productsTab.length/nextState.itemsToPage);
+
+    return {
+      ...nextState,
+      subProductsTab,
+      pageCount,
+    }
+  }
+
+  componentDidUpdate() {
+    console.log('RELOAD');
+    ReactDom.findDOMNode(this).scrollIntoView();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="ProductList">
+          {this.state.subProductsTab.map(product => <ProductListItem key={product.id} product={product} />)}
+        </div>
+        {this.props.page <= this.state.pageCount ? <Pagination className="Pagination" page={this.props.page} pageCount={this.state.pageCount}/> : null}
       </div>
-      {page <= pageCount ? <Pagination className="Pagination" page={page} pageCount={pageCount}/> : null}
-    </div>
-  );
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
