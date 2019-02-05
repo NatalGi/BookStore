ef43e603f64dae5af3068fbf6e1878118f614390
Natalia Ginalska <natalia.ginalska@gmail.com>
2019-02-05 10:55:17 +0100

    Add PWA options: ServiceWorker and icons

--- src/index.js
+++ src/index.js
@@ -7,13 +7,14 @@ import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
-import DevTools from './util/DevTools';
+//import DevTools from './util/DevTools';
+//<DevTools />
+//const store = createStore(reducers, DevTools.instrument());
 
-const store = createStore(reducers, DevTools.instrument());
+const store = createStore(reducers);
 
 ReactDOM.render(
   <Provider store={store}>
-    <DevTools />
     <App store={store}/>
   </Provider>,
   document.getElementById('root')
@@ -22,4 +23,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
